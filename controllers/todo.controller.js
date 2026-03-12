c377b2fb9ad51ce1c7c6da5934c86c72d051a949
margitviigi <margit.viigi@voco.ee>
2026-03-12 13:49:56 +0200

    Create TodoController.createTodo unit test with data and HTTP mocks

---
+++ controllers/todo.controller.js
@@ -0,0 +1,10 @@
+const TodoModel = require('../models/todo.model');
+
+const createTodo = (req, res, next) => {
+    const createdModel = TodoModel.create(req.body);
+    res.status(201).send(createdModel);
+}
+
+module.exports = {
+    createTodo
+}
